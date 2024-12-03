884969ca3b2afe4232f309153fcca2ee074f5780
Paolo Baldini <paolo.baldini.phd@gmail.com>
2024-12-03 13:55:35 +0100

    added lua modules managing different type of damages

---
+++ src/dmg_act_slowed.lua
@@ -0,0 +1,42 @@
+local damage = {}
+
+-- Create a map saying for each actuator if it is broken or not.
+local broken = { false, false }
+
+-- Set the damage to the sensor and actuators. Select at random an actuator to
+-- damage, setting its speed to half the ordinary one.
+function damage.set_damage() -- TODO add ignored value
+    index = math.random(0, 1) + 1
+    broken[index] = true
+end
+
+-- Cycle the sensory inputs and force their values in the BN state.
+-- 
+-- @param[in] inputs: the sensory analog inputs.
+-- @param[in] mapping: the mapping between sensor index and BN node.
+-- @param[in, out] state: the BN state.
+-- @param[in] light_threshold: discriminate if a light is perceived or not.
+-- 
+-- TODO check it pass the state reference and not a copy.
+function damage.perturb_network(inputs, mapping, state, light_threshold)
+    for i = 1, 24 do
+        if inputs[i] > light_threshold then
+            state[mapping[i]] = 1
+        else
+            state[mapping[i]] = 0
+        end
+    end
+end
+
+-- If the actuator is damaged, its maximum speed is halved.
+-- 
+-- @param[in] state: the BN state.
+-- @param[in] mapping: the mapping between actuator index and BN node.
+-- @param[in, out] outputs: the actuator control values to be set.
+-- @param[in] max_speed: the maximum wheels speed.
+function damage.extract_output(state, mapping, outputs, max_speed)
+    outputs[1] = state[mapping[1]] * max_speed / (broken[1] and 2 or 1)
+    outputs[2] = state[mapping[2]] * max_speed / (broken[2] and 2 or 1)
+end
+
+return damage
---
+++ src/dmg_sns_fixed.lua
@@ -0,0 +1,65 @@
+local damage = {}
+
+-- Create a map saying for each light sensor if it is broken or not.
+local broken = {}
+for i = 1, 24 do
+    broken[i] = false
+end
+
+-- Create a map saying for each light sensor its broken value.
+local values = {}
+for i = 1, 24 do
+    values[i] = math.random(0, 1)
+end
+
+-- Set the damage to the sensor and actuators. Select at random a healthy
+-- sensor and start damaging its following ones.
+-- 
+-- @param[in] number_of_faults: the number of damaged sensors.
+function damage.set_damage(number_of_faults)
+    sample = math.random(1, 24)
+    for i = 1, number_of_faults do
+        broken[sample] = true
+        sample = sample + 1
+        if sample > 24 then
+            sample = 1
+        end
+    end
+end
+
+-- A fixed input forces the value on the BN state. Cycle the sensory inputs
+-- and, if not broken, force their reading values in the BN state.
+-- 
+-- @param[in] inputs: the sensory analog inputs.
+-- @param[in] mapping: the mapping between sensor index and BN node.
+-- @param[in, out] state: the BN state.
+-- @param[in] light_threshold: discriminate if a light is perceived or not.
+-- 
+-- TODO check it pass the state reference and not a copy.
+function damage.perturb_network(inputs, mapping, state, light_threshold)
+    for i = 1, 24 do
+        if not broken[i] then
+            if inputs[i] > light_threshold then
+                state[mapping[i]] = 1
+            else
+                state[mapping[i]] = 0
+            end
+        else
+            state[mapping[i]] = values[i]
+        end
+    end
+end
+
+-- The control value for the wheels is simply the output node value times the
+-- maximum speed.
+-- 
+-- @param[in] state: the BN state.
+-- @param[in] mapping: the mapping between actuator index and BN node.
+-- @param[in, out] outputs: the actuator control values to be set.
+-- @param[in] max_speed: the maximum wheels speed.
+function damage.extract_output(state, mapping, outputs, max_speed)
+    outputs[1] = state[mapping[1]] * max_speed
+    outputs[2] = state[mapping[2]] * max_speed
+end
+
+return damage
