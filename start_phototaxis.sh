b38f81c145255b5b4a4dd28badb89dc7f954ed2f
Paolo Baldini <paolo.baldini.phd@gmail.com>
2024-12-03 14:39:48 +0100

    added script to start phototaxis experiments

---
+++ start_phototaxis.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+OUTPUT_PATH="/home/persistent/2024_12_02-output-phototaxis"
+
+# check that the variables of the experiment are set to a non empty value
+[ -z "$BIAS" ] && exit 1
+[ -z "$DAMAGE_MODULE" ] && exit 2
+[ -z "$START_SEED" ] && exit 3
+[ -z "$END_SEED" ] && exit 4
+
+# create an output directory for the experiment results
+mkdir $OUTPUT_PATH
+
+for SEED in `seq $START_SEED $END_SEED`
+do
+    for N_FAULTS in `seq 0 3 24`
+    do
+        # create an instance of the controller for the specific experiment
+        cp phototaxis.lua phototaxis_instance.lua
+
+        # set up the controller with the experiment parameters
+        sed -i "s|££ DAMAGE_MODULE ££|\"$DAMAGE_MODULE\"|" "phototaxis_instance.lua"
+        sed -i "s|££ SEED ££|$SEED|" "phototaxis_instance.lua"
+        sed -i "s|££ BIAS ££|$BIAS|" "phototaxis_instance.lua"
+        sed -i "s|££ NUMBER_OF_FAULTS ££|$N_FAULTS|" "phototaxis_instance.lua"
+
+        # launch the argos3 experiment and save the results to a file
+        argos3 -c run-phototaxis.argos | grep -v INFO > "$OUTPUT_PATH/$DAMAGE_MODULE-$BIAS-$N_FAULTS-$SEED.txt"
+    done
+done
