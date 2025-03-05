118e3ab3fa913dc0de9422884de982cb1b951068
Paolo Baldini <paolo.baldini.phd@gmail.com>
2025-03-05 18:43:23 +0100

    parallelized pt over experiment rather than over replicas

--- parallelize_pt_experiments.sh
+++ parallelize_pt_experiments.sh
@@ -1,14 +1,10 @@
 ROOT_DIR=$(pwd)
 
-# spread 1000 replicas among 10 different cores
-for I in $(seq 0 333 666)
+# spread the experiment across multiple cores
+for DAMAGE_MODULE in "dmg_act_slowed" "dmg_sns_disconnected" "dmg_sns_fixed" "dmg_sns_random"
 do
-    # calculate the seed range that the core should manage
-    SSEED=$I
-    ESEED=$(($I + 332))
-
     # set up the experiment directory
-    WORK_DIR=$(date +"%Y-%m-%d")_experiment/pt/$SSEED-$ESEED
+    WORK_DIR=$(date +"%Y-%m-%d")_experiment/pt/$DAMAGE_MODULE
     mkdir -p                    $WORK_DIR
     cp src/*                    $WORK_DIR
     cp exp/*                    $WORK_DIR
@@ -16,10 +12,7 @@ do
     cd $WORK_DIR
 
     # start the experiments in parallel
-    BIAS=0.79 START_SEED=$SSEED END_SEED=$ESEED DAMAGE_MODULE="dmg_act_slowed"       nohup ./start_pt_experiment.sh &
-    BIAS=0.79 START_SEED=$SSEED END_SEED=$ESEED DAMAGE_MODULE="dmg_sns_disconnected" nohup ./start_pt_experiment.sh &
-    BIAS=0.79 START_SEED=$SSEED END_SEED=$ESEED DAMAGE_MODULE="dmg_sns_fixed"        nohup ./start_pt_experiment.sh &
-    BIAS=0.79 START_SEED=$SSEED END_SEED=$ESEED DAMAGE_MODULE="dmg_sns_random"       nohup ./start_pt_experiment.sh &
+    BIAS=0.79 START_SEED=0 END_SEED=999 DAMAGE_MODULE=$DAMAGE_MODULE nohup ./start_pt_experiment.sh &
 
     cd $ROOT_DIR
 done
