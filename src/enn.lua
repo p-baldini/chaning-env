5e7f07f84c160c23de5a7b434a26cbf1dda0a5c6
Paolo Baldini <paolo.baldini.phd@gmail.com>
2025-07-14 13:44:19 +0200

    now enn works with matrix operations

--- src/enn.lua
+++ src/enn.lua
@@ -1,7 +1,7 @@
 --
 -- Elman Recurrent Neural Network.
 --
-local vcts = require "vectors"
+require "matrix"
 
 local enn = {}
 
@@ -32,182 +32,117 @@ end
 
 -- Create RNN based on the Elman Network.
 -- All the weights are in [0, 1].
--- The starting delay nodes value is 1.
+-- The starting context nodes value is 1.
 function enn.create(incount, hidcount, outcount)
-    local states = {}
-    local weights = {}
+    -- Initialize context units state with 1s
+    local cs = matrix:new(hidcount, 1, 1)
 
-    -- Initialize delay units state with 1s
-    for i = 1, hidcount do
-        states[i] = 1
-    end
+    -- Create the weights matrix from input to hidden nodes
+    -- and randomly set the weights in [0, 1]; consider
+    -- also the bias weights
+    local l1 = matrix:new(hidcount, incount + 1, 0)
+    local l1 = matrix.random(l1, 0, 100)
+    local l1 = matrix.divnum(l1, 100)
 
-    -- Calculate total number of weights:
-    -- ° input X hidden nodes
-    -- ° delayed nodes (one per hidden)
-    -- ° bias (one per hidden)
-    -- ° hidden nodes (one per delayed)
-    -- ° hidden X output nodes
-    -- ° bias (one per output)
-    weightend =             hidcount * incount
-    weightend = weightend + hidcount
-    weightend = weightend + hidcount
-    weightend = weightend + hidcount
-    weightend = weightend + hidcount * outcount
-    weightend = weightend + outcount
+    -- Create the weights matrix from context to hidden
+    -- nodes and randomly set the weights in [0, 1]
+    local lc = matrix:new(hidcount, 1, 0)
+    local lc = matrix.random(lc, 0, 100)
+    local lc = matrix.divnum(lc, 100)
 
-    -- Initialize input-to-hidden weights
-    for i = 1, weightend do
-        weights[i] = math.random(0, 100) / 100.0
-    end
+    -- Create the weights matrix from hidden to output
+    -- nodes and randomly set the weights in [0, 1];
+    -- consider also the bias weights
+    local l2 = matrix:new(outcount, hidcount + 1, 0)
+    local l2 = matrix.random(l2, 0, 100)
+    local l2 = matrix.divnum(l2, 100)
 
-    -- Set context units in-weights to 1 (standard Elman model)
-    -- start_idx = (incount + 2) * hidcount + 1
-    -- finish_idx = start_idx + hidcount
-    -- for i = start_idx, finish_idx do
-    --     weights[i] = 1
-    -- end
-    -- -- Set context units out-weights to 1 NON-STANDARD
-    -- for i = 1, hidcount do
-    --     weights[i * (incount + 2)] = 1
-    -- end
-
-    return {
-        incount = incount,
-        hidcount = hidcount,
-        outcount = outcount,
-        states = states,
-        weights = weights
-    }
+    return { l1 = l1, lc = lc, cs = cs, l2 = l2 }
 end
 
 
 -- Compute the output according to the input
 function enn.compute(enn, inputs)
-    local outputs = {}
-    local hiddens = {}
-
-    incount = enn[1]
-    hidcount = enn[2]
-    outcount = enn[3]
-    states = enn[4]
-    weights = enn[5]
-
     -- Check that the number of inputs match with
     -- the number of input nodes, otherwise exit
-    if #inputs ~= incount then
+    if #inputs + 1 ~= #enn.l1[1] then
         os.exit(2)
     end
 
-    -- Compute the state of the hidden nodes
-    for i = 1, hidcount do
-        -- Get the slice of the weight array
-        -- belonging to the hidden node,
-        -- including inputs, delay, and bias
-        start_idx = (i - 1) * (incount + 2) + 1
-        finish_idx = start_idx + incount + 2 - 1
+    -- Set the input and bias values in a column matrix
+    local is = { inputs }
+    is[1][#inputs + 1] = 1
+    is = matrix.transpose(is)
 
-        -- Set the delay and the bias values of
-        -- the hidden node after the inputs
-        inputs[incount + 1] = states[i]
-        inputs[incount + 2] = 0 -- % TODO 0 sembra meglio...
+    -- Compute the value of the hidden nodes according to
+    -- the inputs and bias, and then add the value of
+    -- context nodes
+    local hs = matrix.add(
+        matrix.mul(enn.l1, is),
+        matrix.mul(enn.lc, enn.cs)
+    )
 
-        -- Compute the state according to the inputs, delay, and bias
-        node_weights = vcts.sub(weights, start_idx, finish_idx)
-        hiddens[i] = vcts.rowXcol(node_weights, inputs)
-
-        -- Apply the sigmoid activation function
-        hiddens[i] = sigmoid(hiddens[i])
-
-        -- Update the delay nodes state
-        delay_weight_idx = (incount + 2) * hidcount + i
-        states[i] = hiddens[i] * weights[delay_weight_idx]
+    -- Apply the activation function on the hidden nodes
+    for i = 1, #hs[1] do
+        hs[1][i] = sigmoid(hs[1][i])
     end
 
-    -- Compute the state of output nodes
-    for i = 1, outcount do
-        -- Get the slice of the weight array
-        -- belonging to the output node,
-        -- including inputs, and bias.
-        -- The output weights start after the
-        -- hidden and delay weights.
-        start_idx = (incount + 2) * hidcount + hidcount
-        start_idx = start_idx + (i - 1) * (hidcount + 1) + 1
-        finish_idx = start_idx + hidcount
+    -- Copy the value of the hidden nodes to the context
+    enn.cs = hs
 
-        -- Set the bias values of the output node
-        -- after the inputs
-        hiddens[hidcount + 1] = 0 -- % TODO 0 sembra meglio...
+    -- Compute the value of the output nodes according to
+    -- the hidden nodes and bias
+    hs[#hs + 1] = { 1 }
+    local os = matrix.mul(enn.l2, hs)
 
-        -- Compute the state according to the inputs, and bias
-        node_weights = vcts.sub(weights, start_idx, finish_idx)
-        outputs[i] = vcts.rowXcol(node_weights, hiddens)
-
-        -- Apply the sigmoid activation function
-        outputs[i] = sigmoid(outputs[i])
+    -- Apply the activation function on the hidden nodes
+    for i = 1, #os[1] do
+        os[1][i] = sigmoid(os[1][i])
     end
 
-    enn = {
-        incount,
-        hidcount,
-        outcount,
-        states,
-        weights
-    }
-    return enn, outputs
+    return enn, { os[1][1], os[2][1] }
 end
 
 
 -- Random change the weights of the RNN, but keep them in -1, 1
 function enn.change(enn, p, perf)
-    hidcount, outcount = enn[2], enn[3]
-
-    start_idx = #enn[5] - (hidcount + 1) * outcount
-
     -- Multiplier to increase intensity of perturbation with decrease of performance
     mult = math.exp(-5 * perf)
 
-    for i = start_idx, #enn[5] do
-        if math.random() <= p then
-            enn[5][i] = enn[5][i] + mult * math.random(-50, 50) / 100.0
-            enn[5][i] = math.max(0, enn[5][i])
-            enn[5][i] = math.min(1, enn[5][i])
+    for i = 1, #enn.l2 do
+        for j = 1, #enn.l2[1] do
+            if math.random() <= p then
+                enn.l2[i][j] = enn.l2[i][j] + mult * math.random(-100, 100) / 100.0
+                enn.l2[i][j] = math.max(0, enn.l2[i][j])
+                enn.l2[i][j] = math.min(1, enn.l2[i][j])
+            end
         end
     end
 end
 
 
 -- Copy the RNN weights
-function enn.copy(old)
-    local new = {
-        incount = old.incount,
-        hidcount = old.hidcount,
-        outcount = old.outcount,
-        states = {}, weights = {}
+function enn.copy(enn)
+    return {
+        l1 = matrix.copy(enn.l1),
+        lc = matrix.copy(enn.lc),
+        cs = matrix.copy(enn.cs),
+        l2 = matrix.copy(enn.l2)
     }
-
-    for i = 1, #old.states do
-        new.states[i] = old.states[i]
-    end
-    for i = 1, #old.weights do
-        new.weights[i] = old.weights[i]
-    end
-
-    return new
 end
 
 
 -- Print RNN
-function enn.print(net)
-    io.write(
-        "Elman network shape: ",
-        net.incount, "x", net.hidcount, "x", net.outcount, " ",
-        "-- Weights: "
-    )
-    for i = 1,#net.weights do
-        io.write(net.weights[i], " ")
-    end
-    print()
+function enn.print(enn)
+    print("Network:")
+    print("l1")
+    matrix.print(enn.l1)
+    print("lc")
+    matrix.print(matrix.transpose(enn.lc))
+    print("cs")
+    matrix.print(matrix.transpose(enn.cs))
+    print("l2")
+    matrix.print(enn.l2)
 end
 
 
